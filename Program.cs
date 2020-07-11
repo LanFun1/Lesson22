385ba469ac8240a0cfd36836812a73533178b284
LanFun1 <66485374+LanFun1@users.noreply.github.com>
2020-07-11 10:40:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string codeWord = "";
+            while(codeWord != "exit")
+            {
+                Console.WriteLine("Введите кодовое слово.");
+                codeWord = Console.ReadLine();
+            }
+        }
+    }
+}
